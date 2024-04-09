c82e048cbe989a4c1aa0b31e1f8ea90db96e95d5
Farzeen Qaiser <fiziqaiser@gmail.com>
2024-04-09 16:01:28 +0500

    Add Library

---
+++ Shared/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ULMS.Shared.Models;
+
+public partial class Book
+{
+    public long Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string? Author { get; set; }
+
+    public string? Uploadedby { get; set; }
+
+    public string? Url { get; set; }
+}

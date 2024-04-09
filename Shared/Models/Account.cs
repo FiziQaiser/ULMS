e09f8dfb8fa15eeaa96aeb4d3ae61a7a2f276a5d
Farzeen Qaiser <fiziqaiser@gmail.com>
2024-04-09 06:10:57 +0500

    Account Model Ready

---
+++ Shared/Models/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ULMS.Shared.Models;
+
+public partial class Account
+{
+    public long Id { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public string? Password { get; set; }
+
+    public string? Role { get; set; }
+
+    public string? Name { get; set; }
+}

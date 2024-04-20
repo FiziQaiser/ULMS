a6fa0111af6c4d04c5037b3dd60e184e42642fae
Farzeen Qaiser <fiziqaiser@gmail.com>
2024-04-20 11:12:56 +0500

    View Join Classroom Added

---
+++ Shared/Models/Classroom.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ULMS.Shared.Models;
+
+public partial class Classroom
+{
+    public long ClassroomId { get; set; }
+
+    public long UserId { get; set; }
+
+    public string? ClassroomName { get; set; }
+}
